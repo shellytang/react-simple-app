deb3a335ca1158a7023db08b25ecd88f15f4f26e
Shelly Tang <shellytang@gmail.com>
2017-08-11 08:40:16 -0700

    refactored to class component, added input handler, and initialized state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-  return <input />
+class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+    
+    this.state = { term: ''};
+  }
+
+  render () {
+    return <input onChange={event => console.log(event.target.value)}/>
+  }
 }
 
 export default SearchBar;
