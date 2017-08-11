59635f8fb18832a127adabe00f386268146d56fd
Shelly Tang <shellytang@gmail.com>
2017-08-11 08:55:09 -0700

    updated to include controlled input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,12 +4,19 @@ class SearchBar extends Component {
 
   constructor(props) {
     super(props);
-    
+
     this.state = { term: ''};
   }
 
   render () {
-    return <input onChange={event => console.log(event.target.value)}/>
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({term: event.target.value})}
+        />
+      </div>
+    )
   }
 }
 
